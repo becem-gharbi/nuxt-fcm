dbf3c9888f52f9dfbfd6157aafc9353818fdc4cf
becem-gharbi <99251251+becem-gharbi@users.noreply.github.com>
2023-04-03 14:47:48 +0100

    refactor: create client only FcmTest component

--- playground/app.vue
+++ playground/app.vue
@@ -1,24 +1,5 @@
 <template>
-  <div>
-    <ClientOnly>
-      <h2>Registration token</h2>
-      <p>{{ $fcm.registrationToken }}</p>
-
-      <h2 v-if="message">New message</h2>
-      <p>{{ message }}</p>
-    </ClientOnly>
-  </div>
+  <ClientOnly>
+    <FcmTest />
+  </ClientOnly>
 </template>
-
-<script setup>
-import { useNuxtApp, ref } from '#imports';
-
-const message = ref("")
-
-if (process.client) {
-  const { $fcm } = useNuxtApp()
-
-  $fcm.onMessage((payload) => message.value = payload)
-}
-
-</script>
---
+++ playground/components/FcmTest.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2>Registration token</h2>
+        <p>{{ $fcm.registrationToken }}</p>
+
+        <h2 v-if="message">New message</h2>
+        <p>{{ message }}</p>
+    </div>
+</template>
+  
+<script setup>
+import { useNuxtApp, ref } from '#imports';
+
+const message = ref("")
+
+const { $fcm } = useNuxtApp()
+
+$fcm.onMessage((payload) => message.value = payload)
+
+</script>
+  
