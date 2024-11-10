15a6c263fd4ec718c7fdd2dc2114f398a6571df7
becem-gharbi <becem.gharbi@live.com>
2024-11-10 08:31:05 +0100

    chore(playground): update default options

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -4,9 +4,9 @@ export default defineNuxtConfig({
   modules: ['../src/module'],
 
   fcm: {
-    firebaseConfig: JSON.parse(process.env.FCM_FIREBASE_CONFIG!),
-    vapidKey: process.env.FCM_VAPID_KEY,
-    serviceAccount: JSON.parse(process.env.FCM_SERVICE_ACCOUNT!),
+    firebaseConfig: {},
+    vapidKey: '*',
+    serviceAccount: {},
     serviceWorkerScript: `
       messaging.onBackgroundMessage((payload) => {
         console.log("[firebase-messaging-sw.js] Received background message ", payload);
